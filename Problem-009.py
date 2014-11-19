0fa003d1a5b21d9b0d85e03d9c0523cd113b2ae7
dfoyle <damian.foyle@gmail.com>
2014-11-19 21:19:00 +0200

    Problem 009

--- Problem-009.py
+++ Problem-009.py
@@ -19,66 +19,68 @@
     :url:
     https://projecteuler.net/problem=9
 
-    :answer:
-    :time:
+    :answer: 31875000
+    :time: 0.3s
 
     :author:
     :updated: 2014-11-19
 """
 from time import time
-from math import sqrt
 t = time()
 
 
-def sum_squares(a, b):
-    """ Sum of squares.
+def sroot(n):
+    """ Check if n's square root is a real number.
 
-        >>> sum_squares(2, 3)
-        13
+        >>> sroot(25)
+        True
+
+        >>> sroot(26)
+        False
     """
 
-    return a*a + b*b
+    return int(n ** 0.5) == n ** 0.5
 
 
-def find_c(a, b):
+def find_it():
+    """ Find a, b, c where:
+        a + b + c = 1000
+    """
+
+    # It's unlikely of a, b, c to be higher
+    # than 1000/2
+    limit = 500
+
+    found = ()
+
+    i = 1
+    while i < limit:
+        j = 1
+        while j < limit:
+            k = find_k(i, j)
+            if i + j + k == 1000:
+                found = (i, j, k)
+            j += 1
+        i += 1
+
+    product = 1
+
+    for number in found:
+        product *= number
+
+    return int(product)
+
+
+def find_k(i, j):
     """ Find the long side of the
         triangle.
     """
 
-    result = sqrt(sum_squares(a, b))
+    result = ((i * i) + (j * j)) ** 0.5
     return result
 
 
-def find_it(n):
-    """ n:1000 in this case.
-    """
-
-    # a, b, c should be smaller then
-    # half of their sum.
-    limit = n / 2
-    n = float(n)
-
-    checked = []
-
-    for a in range(1, limit):
-        for b in range(1, limit):
-            if (a, b) in checked or (b, a) in checked:
-                print "Passing: \t", (a, b)
-                pass
-            else:
-                print "Checking: \t", (a, b)
-                checked.append((a, b))
-                c = find_c(a, b)
-                if c.is_integer():
-                    total = a + b + c
-                    if total == n:
-                        found = (a, b, c)
-                        print "c : ", c, " - ", a + b + c
-                        break
-    print found
-
-
-find_it(1000)
+print find_it()
 print "Finished in:", time() - t
 
 
